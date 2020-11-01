5775891f7c8241bfa6c1239f36dfe56eb4eb7378
gamliel <gamlielchemla@gmail.com>
2020-11-01 16:37:03 +0200

    myapp

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path'); 
 
 
 function midll (req,res,next){
@@ -8,13 +9,30 @@ function midll (req,res,next){
 }
 function midll2 (req,res,next){
   res.send("HELLO")
-  res.sendFile("../public/images/20190526_203245.jpg",err=>console.log("s"))
-  res.download('../public/images/20190526_203245.jpg')
-  console.log("2er midlle");
+
+  app.get('/', ); 
+  // res.sendFile("../public/images/20190526_203245.jpg",err=>console.log("s"))
+  // res.download('../public/images/20190526_203245.jpg')
+  // console.log("2er midlle");
 }
 
 /* GET users listing. */
-router.get('/', midll,midll2)
+router.get('/',function(req, res, next){ 
+
+  console.log("__dirname",__dirname);
+  var options = { 
+      root: path.join(__dirname) 
+  }; 
+    
+  var fileName = 'Hello.txt'; 
+  res.sendFile(fileName, options, function (err) { 
+      if (err) { 
+          next(err); 
+      } else { 
+          console.log('Sent:', fileName); 
+      } 
+  }); 
+})
 //  {
   // res.send('respond with a resource');
   // next()
